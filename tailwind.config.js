fca2086eaf0a955520b16863e578953e48a94f9d
rubens-fs <rubens.frota@gmail.com>
2023-08-02 11:06:53 -0300

    Initial Structure v1.0 Tailwind files

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+const { Container } = require('postcss');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  important: true,
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['poppins', 'sans-serif']
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          'sm': "512px",
+          'md': "720px",
+          'lg': "1024px",
+          'xl': "1140px",
+          '2xl': "1320px"
+        }
+      },
+      backgroundImage: {
+        'hero-img': "url('../src/images/bg-image.jpg')"
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
+
